54a93db316fa3864ef835cdfa18a7e273d21af4e
Pierluigi Frascogna <pyrusf23@gmail.com>
2021-12-21 23:57:25 +0100

    Creato il model dell'User e del db

--- models/User.py
+++ models/User.py
@@ -1,4 +1,4 @@
-from models import db
+from models.db import db
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
@@ -7,6 +7,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False, unique=True) # Password sha256(64)
+    email = db.Column(db.String(80), nullable=True, unique=True)
 
     def __init__(self, username, password):
         self.username = username
