62d9457693d1163506fdfefc83a8120e9d8e53ca
Pierluigi Frascogna <pyrusf23@gmail.com>
2021-12-21 23:56:24 +0100

    Modificato l'Init dell'app

--- __init__.py
+++ __init__.py
@@ -1,12 +1,29 @@
 from flask import Flask
 
+#def create_app():
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
 from settings import Debug
 app.config.from_object(Debug)
 
+from models.db import db
+db.init_app(app)
+
+from views.main import main
+app.register_blueprint(main)
+
+from views.authentication import auth
+app.register_blueprint(auth)
+
+from models.User import User
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#return app
 app.run()
