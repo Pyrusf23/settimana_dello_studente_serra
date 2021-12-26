fbb05f04db35d186ee56df46cc3e934228ad275d
Christian <denisichristian05@libero.it>
2021-12-26 22:53:24 +0100

    Aggiunto lo script per le migrazioni su database e sorpresa nel footer

---
+++ migration_script.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+# Graffetta
+
+app = Flask(__name__)
+
+from settings import Debug
+app.config.from_object(Debug)
+
+from models.db import db
+db.init_app(app)
+
+# Importa qui i nuovi modelli
+from models import User
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == "__main__":
+    manager.run()
+
+####################
+#                  #
+#  Comandi         #
+#  by De Nisi      #
+#                  #
+####################
+
+# Migrazione
+# python migration_script.py db migrate
+
+# Esportazione su db
+# python migration_script.py db upgrade
