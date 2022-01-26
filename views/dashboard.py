d8b6240af25d8d3415e0bb61248eb62edac51d89
Pierluigi Frascogna <pyrusf23@gmail.com>
2022-01-26 05:16:39 +0100

    Modificate le attività in tutti i file: seguendo l'idea di dover fare una pagina per l'orario ed una pagina per le attività ho rinominato i file che stavo attualmente usando in 'orario' e ho modificato dove necessario. Inoltre ho modificato dashboard.py per renderlo più leggibile e migliorarne la 'logica'. Sono le 5:15 del 26 gennaio 2022 e io mi sto facendo molte domande sulle mie scelte di vita...

--- views/dashboard.py
+++ views/dashboard.py
@@ -9,27 +9,45 @@ from flask_login import login_user, current_user, logout_user, login_required
 dashboard = Blueprint('dashboard', __name__)
 
 # Routing
-@dashboard.route("/attivita")
+@dashboard.route("/orario")
 @login_required
-def activities():
+def timetable():
+    orario=[[0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]] # Chri mi spiace per l'ineleganza ma purtroppo dovevo necessariamente fare questa matrice perché sennò jinja impazziva
+            # Questa matrice segue quest'ordine giorno[ora[placeholder]]
+    giorni = {9:0, 10:1, 11:2, 14:3, 15:4, 16:5} # Dizionario che restituisce l'indice dei giorni della matrice in base al giorno del mese
+
     materie_query = """SELECT orari.giorno, orari.ora, materie.nome_materia
-    FROM utenti, materie, orari, orari_materie_classi
-    WHERE orari.id=orari_materie_classi.id_orario
-	AND materie.id=orari_materie_classi.id_materia
-	AND orari_materie_classi.id_classe=utenti.id_classe
-	AND utenti.id=""" + "'" + str(current_user.id) + "'"
-    materie_result = execute_query(materie_query).all()
+        FROM utenti, materie, orari, orari_materie_classi
+        WHERE orari.id=orari_materie_classi.id_orario
+            AND materie.id=orari_materie_classi.id_materia
+            AND orari_materie_classi.id_classe=utenti.id_classe
+            AND utenti.id=""" + "'" + str(current_user.id) + "'"
+    materie_result = execute_query(materie_query).all() # Restituisce una tupla (giornoDelMese, ora, materia)
     # print(materie_result)
-    materie=[[[], [], [], [], [], [], []], [[], [], [], [], [], [], []], [[], [], [], [], [], [], []], [[], [], [], [], [], [], []], [[], [], [], [], [], [], []], [[], [], [], [], [], [], []]]
-    giorni = {9:0, 10:1, 11:2, 14:3, 15:4, 16:5}
     for materia in materie_result:
-        giorno = giorni[materia[0]]
-        ora = (materia[1])-1
-        materie[giorno][ora] = materia[2]
-    # print(materie)
-    
-    # attivita_query = "placeholder"
-    # attivita_result = execute_query(attivita_query).all()
+        giorno = giorni[materia[0]] # Traduce il giorno del mese da query nell'indice appropriato per la matrice
+        ora = (materia[1])-1 # Riduce di uno l'ora del giorno per renderlo appropriato alla matrice
+        orario[giorno][ora] = materia[2]
+    # print(orario)
 
+    attivita_query = """SELECT orari.giorno, orari.ora, attivita.nome
+        FROM utenti, orari, attivita, utenti_attivita, attivita_orari
+        WHERE orari.id=attivita_orari.id_orario
+            AND attivita.id=attivita_orari.id_attivita
+            AND attivita_orari.id=utenti_attivita.id_attivita_orario
+            AND utenti_attivita.id_utente=utenti.id
+            AND utenti.id=""" + "'" + str(current_user.id) + "'"
+    attivita_result = execute_query(attivita_query).all() # Restituisce una tupla (giornoDelMese, ora, attivita)
+    # print(attivita_result)
+    for attivita in attivita_result:
+        giorno = giorni[attivita[0]]
+        ora = (attivita[1])-1
+        orario[giorno][ora] = attivita[2] # Sostituisce la materia dove c'è un'attività
+    # print(orario)
 
-    return render_template("attivita.html", user=current_user.email, materie=materie)
+    return render_template("orario.html", user=current_user.email, orario=orario)
